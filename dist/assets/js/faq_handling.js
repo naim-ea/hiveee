6d34246ccea0a0c0351d617fbf369d165acc1558
naim-ea <toast.nine@gmail.com>
2017-06-17 23:27:23 +0200

    :art: Some responsive

--- dist/assets/js/faq_handling.js
+++ dist/assets/js/faq_handling.js
@@ -1,10 +1,12 @@
+"use strict";
+
 if (document.addEventListener) {
     document.addEventListener("click", function (event) {
-        
         var targetElement = event.target || event.srcElement;
+        console.log(targetElement);
         var parent = targetElement.parentNode;
+        console.log(parent);
         var answer = parent.querySelector(".about-hiveee-answer");
-        
         if (answer.classList.contains("displayed-question")) {
             answer.classList.remove("displayed-question");
         } else {
@@ -15,16 +17,6 @@ if (document.addEventListener) {
 } else if (document.attachEvent) {
     document.attachEvent("onclick", function () {
         var targetElement = event.target || event.srcElement;
-        
-        var targetElement = event.target || event.srcElement;
-        var parent = targetElement.parentNode;
-        var answer = parent.querySelector(".about-hiveee-answer");
-        
-        if (answer.classList.contains("displayed-question")) {
-            answer.classList.remove("displayed-question");
-        } else {
-            console.log(answer);
-            answer.classList.add("displayed-question");
-        }
+        console.log(targetElement);
     });
-} 
+}
