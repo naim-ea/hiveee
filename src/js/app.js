db4de60c6098884dea326530e631cd7abb0e7a17
naim-ea <toast.nine@gmail.com>
2017-06-17 20:40:32 +0200

    :sparkles: Added hamburger menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
-console.log('Hello World')
+var header = document.querySelector("header");
+var header_links = document.querySelectorAll(".header-nav a");
+var hamburger_menu = document.querySelector(".hamburger-menu");
+var hamburger_menu_bar = document.querySelectorAll(".hamburger-menu .bar");
+var header_nav_from_hamburger = document.querySelector(".header-nav-from-hamburger");
+
+window.addEventListener("scroll", function () {
+    var sTop = (this.pageYOffset || document.scrollTop) - (document.clientTop || 0);
+    if (sTop > 150) {
+        header.classList.add("white-header");
+    } else {
+        header.classList.remove("white-header");
+    }
+});
+
+hamburger_menu.addEventListener("click", function () {
+    for (var j = 0; j < hamburger_menu_bar.length; j++) {
+        if (hamburger_menu_bar[j].classList.contains("animate")) {
+            header_nav_from_hamburger.classList.remove("open");
+            hamburger_menu_bar[j].classList.remove("animate");
+        }
+        else{
+            header_nav_from_hamburger.classList.add("open");
+            hamburger_menu_bar[j].classList.add("animate");
+        }
+    }
+})
