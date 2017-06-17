0d45762a48dd45bb84e701f48039382961283a2c
samuelmormin <samuel.mormin@hetic.net>
2017-06-17 22:02:18 +0200

    Fixing conflicts

--- dist/assets/js/faq_handling.js
+++ dist/assets/js/faq_handling.js
@@ -1,28 +1,30 @@
-"use strict";
-
-var header = document.querySelector("header");
-var header_links = document.querySelectorAll(".header-nav a");
-var hamburger_menu = document.querySelector(".hamburger-menu");
-var hamburger_menu_bar = document.querySelectorAll(".hamburger-menu .bar");
-var header_nav_from_hamburger = document.querySelector(".header-nav-from-hamburger");
-
-window.addEventListener("scroll", function () {
-    var sTop = (this.pageYOffset || document.scrollTop) - (document.clientTop || 0);
-    if (sTop > 150) {
-        header.classList.add("white-header");
-    } else {
-        header.classList.remove("white-header");
-    }
-});
-
-hamburger_menu.addEventListener("click", function () {
-    for (var j = 0; j < hamburger_menu_bar.length; j++) {
-        if (hamburger_menu_bar[j].classList.contains("animate")) {
-            header_nav_from_hamburger.classList.remove("open");
-            hamburger_menu_bar[j].classList.remove("animate");
+if (document.addEventListener) {
+    document.addEventListener("click", function (event) {
+        
+        var targetElement = event.target || event.srcElement;
+        var parent = targetElement.parentNode;
+        var answer = parent.querySelector(".about-hiveee-answer");
+        
+        if (answer.classList.contains("displayed-question")) {
+            answer.classList.remove("displayed-question");
         } else {
-            header_nav_from_hamburger.classList.add("open");
-            hamburger_menu_bar[j].classList.add("animate");
+            console.log(answer);
+            answer.classList.add("displayed-question");
         }
-    }
-});
+    });
+} else if (document.attachEvent) {
+    document.attachEvent("onclick", function () {
+        var targetElement = event.target || event.srcElement;
+        
+        var targetElement = event.target || event.srcElement;
+        var parent = targetElement.parentNode;
+        var answer = parent.querySelector(".about-hiveee-answer");
+        
+        if (answer.classList.contains("displayed-question")) {
+            answer.classList.remove("displayed-question");
+        } else {
+            console.log(answer);
+            answer.classList.add("displayed-question");
+        }
+    });
+} 
